a04d2d514580be3dfa84dd8e42aec2ab49eb7338
violin788788 <violin788788@gmail.com>
2026-01-06 13:48:57 -0500

    Remove textiden/run.py

--- textiden/run.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r"A:\Program Files\Tesseract-OCR\tesseract.exe"
-image = Image.open("fdr.png")
-text = pytesseract.image_to_string(image)
-print(text)
