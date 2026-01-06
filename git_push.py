3720643010313af621f2ebaef6135baee6690aa3
violin788788 <violin788788@gmail.com>
2026-01-06 13:34:55 -0500

    Update git_push.py

--- git_push.py
+++ git_push.py
@@ -1,5 +1,5 @@
 from github import Github
-import os
+import os,sys
 
 repo_path = r"A:\Users\-\0code"
 token_file = r"A:\Users\-\0code\git_token\git_token.txt"
@@ -10,10 +10,12 @@ with open(token_file,"r") as f: token = f.read().strip()
 g = Github(token)
 repo = g.get_user().get_repo(repo_name)
 
+
+num_files = len(os.listdir(repo_path))
 count = 0
 for f in os.listdir(repo_path):
     count=count+1
-    print(count,len(repo_path))
+    print(count,num_files)
     full_path = os.path.join(repo_path,f)
     if os.path.isfile(full_path) and "." in f:
         with open(full_path,"r",encoding="utf-8",errors="ignore") as file: content = file.read()
