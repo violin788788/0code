b1bb1280e14209befb229c4c63a78477f5458592
violin788788 <violin788788@gmail.com>
2026-01-07 13:45:24 -0500

    Sync onedir.py

--- onedir.py
+++ onedir.py
@@ -1,9 +1,27 @@
 import PyInstaller.__main__
 import os
+
+def generate_onedir(file_name):
+    #file_name = "screenshot"
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    PyInstaller.__main__.run([script,'--onedir','--name=' + file_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', file_name)
+    os.startfile(dist_folder)
+
 file_name = "screenshot"
-file_extension = ".py"
-icon_name = file_name + ".ico"
-script = file_name + file_extension
-PyInstaller.__main__.run([script,'--onedir','--name=' + file_name,'--icon=' + icon_name,'--windowed'])
-dist_folder = os.path.join(os.getcwd(), 'dist', file_name)
-os.startfile(dist_folder)
+generate_onedir(file_name)
+
+
+def generate_onefile(file_name):
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    out_name = file_name+"-installer"
+    PyInstaller.__main__.run([script,'--onefile','--name=' + out_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', out_name)
+    os.startfile(dist_folder)
+
+file_name = "screenshot"
+generate_onefile(file_name)
