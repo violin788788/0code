5516cfb94f1626cb3fa5ad8d6c73d0359441f9ee
violin788788 <violin788788@gmail.com>
2026-01-07 13:45:42 -0500

    Sync onefile.py

---
+++ onefile.py
@@ -0,0 +1,14 @@
+import PyInstaller.__main__
+import os
+
+def generate_onefile(file_name):
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    out_name = file_name+"-installer"
+    PyInstaller.__main__.run([script,'--onefile','--name=' + out_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', out_name)
+    os.startfile(dist_folder)
+
+file_name = "screenshot"
+generate_onefile(file_name)
