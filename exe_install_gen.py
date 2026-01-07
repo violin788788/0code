68b20f3d63e7d05d5a20b95b3e780f9d38ca753a
violin788788 <violin788788@gmail.com>
2026-01-07 13:45:15 -0500

    Sync exe_install_gen.py

--- exe_install_gen.py
+++ exe_install_gen.py
@@ -1,18 +1,24 @@
 import PyInstaller.__main__
-import sys
 import os
 
-# Set the script and icon directly
-script_path = 'screenshot.pyw'
-icon_path = 'screenshot.ico'
+def generate_onedir(file_name):
+    #file_name = "screenshot"
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    PyInstaller.__main__.run([script,'--onedir','--name=' + file_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', file_name)
+    os.startfile(dist_folder)
 
-# Generate output name with _installer
-base_name = os.path.splitext(os.path.basename(script_path))[0] + "_installer"
+def generate_onefile(file_name):
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    out_name = file_name+"-installer"
+    PyInstaller.__main__.run([script,'--onefile','--name=' + out_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', out_name)
+    os.startfile(dist_folder)
 
-PyInstaller.__main__.run([
-    script_path,
-    '--onedir',            # create folder with files
-    '--name=' + base_name,
-    '--icon=' + icon_path,
-    '--windowed'           # GUI app, no console
-])
+file_name = "screenshot"
+generate_onedir(file_name)
+generate_onefile(file_name)
