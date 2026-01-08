ef1583a951e14c525e21f8e4a260db9c29a1f0b4
violin788788 <violin788788@gmail.com>
2026-01-08 16:27:27 -0500

    Sync exe_install_generator.py

---
+++ exe_install_generator.py
@@ -0,0 +1,30 @@
+import PyInstaller.__main__
+import os
+
+def generate_onedir(file_name):
+    #file_name = "screenshot"
+    file_extension = ".py"
+    icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    PyInstaller.__main__.run([script,'--onedir','--name=' + file_name,'--icon=' + icon_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', file_name)
+    os.startfile(dist_folder)
+
+def generate_onefile(file_name):
+    file_extension = ".py"
+    #icon_name = file_name + ".ico"
+    script = file_name + file_extension
+    out_name = file_name+"-installer"
+    #PyInstaller.__main__.run([script,'--onefile','--name=' + out_name,'--icon=' + icon_name,'--windowed'])
+    PyInstaller.__main__.run([script,'--onefile','--name=' + out_name,'--windowed'])
+    dist_folder = os.path.join(os.getcwd(), 'dist', out_name)
+    os.startfile(dist_folder)
+
+#def copy_to_program_files(file_name):
+    
+    
+    #A:\Users\-\0code\dist\screenshot
+
+file_name = "copy_files"
+#generate_onedir(file_name)
+generate_onefile(file_name)
