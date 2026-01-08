9c4f3b7edeed16e2b1d2f15774ceb1052aaa4860
violin788788 <violin788788@gmail.com>
2026-01-08 16:27:16 -0500

    Sync copy_files.py

---
+++ copy_files.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+def copy_all_files(src_dir, dst_dir):
+    os.makedirs(dst_dir, exist_ok=True)
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dst_path = os.path.join(dst_dir, item)
+
+        if os.path.isdir(src_path):
+            #shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
+            shutil.copytree(src_path, dst_path)
+        elif os.path.isfile(src_path):
+            shutil.copy2(src_path, dst_path)
+        print (item)
+
+src = r"A:\Users\-\0code\dist\screenshot"
+dst = r"A:\Program Files\screenshot"
+
+copy_all_files(src, dst)
+
+
+"""
+import os,subprocess,sys
+folder="dist\\screenshot"
+script="copy_files.py"
+cmd=f'pyinstaller --onefile --add-data "{folder};{folder}" {script}'
+subprocess.run(cmd,shell=True)
+"""
