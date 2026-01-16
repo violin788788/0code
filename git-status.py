dcc49ba294a5842e00960d9b4000127a8e437eae
Your Name <you@example.com>
2026-01-16 10:25:34 -0500

    update/create/delete files+folders

--- git-status.py
+++ git-status.py
@@ -1,9 +1,10 @@
 import subprocess
 import os
 cwd=os.getcwd()
-repos=["info34","vote34","trading"]
+repos=["info34","vote34","trading",""]
 commands=["git status"]
 for repo in repos:
+    print("--------------------------------------------")
     check=os.path.join(cwd,repo)
     print("--- Running in",check,"---")
     for command in commands:
